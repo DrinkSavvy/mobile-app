63d0442b40661de99704b164d1d8de9dd3d9c8ff
Patrick Pistor <patrick@photure.app>
2023-02-10 16:54:23 -0500

    Remove spirokit and switch to nativebase :(

--- App.tsx
+++ App.tsx
@@ -1,27 +1,21 @@
 import React from "react"
-import { SpiroKitProvider, usePoppins, useSpiroKitTheme } from "@spirokit/core"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NativeBaseProvider } from "native-base"
 
 import { supabase } from "./api/supabase"
 import LoginScreen from "./screens/Login/Login"
 
-const myTheme = useSpiroKitTheme()
-
 const Stack = createNativeStackNavigator()
 
 export default function App() {
-  const fontLoaded = usePoppins()
-
-  if (!fontLoaded) return <></>
-
   return (
-    <SpiroKitProvider theme={myTheme}>
+    <NativeBaseProvider>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen name="Login" component={LoginScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-    </SpiroKitProvider>
+    </NativeBaseProvider>
   )
 }
--- screens/Login/Login.tsx
+++ screens/Login/Login.tsx
@@ -1,26 +1,28 @@
+import { Box, Center, Input, VStack } from "native-base"
 import React from "react"
-import { Center, VStack, Image, Input, Box } from "@spirokit/core"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
 
 export default function LoginScreen() {
   return (
-    <Center flex={1}>
-      <VStack space={4} alignItems="center">
-        <Box width="100%">
-          <Input
-            placeholder="Email"
-            variant="filled"
-            width="100%"
-            mb={2}
-            _focus={{ borderColor: "primary.500" }}
-          />
-          <Input
-            placeholder="Password"
-            variant="filled"
-            width="100%"
-            _focus={{ borderColor: "primary.500" }}
-          />
-        </Box>
-      </VStack>
-    </Center>
+    <KeyboardAwareScrollView>
+      <Center flex={1}>
+        <VStack space={4} width="100%" maxWidth="400px">
+          <Box>
+            <Input
+              placeholder="Email"
+              width="100%"
+              marginBottom={2}
+              _focus={{ borderColor: "primary.500" }}
+            />
+            <Input
+              placeholder="Password"
+              width="100%"
+              marginBottom={2}
+              _focus={{ borderColor: "primary.500" }}
+            />
+          </Box>
+        </VStack>
+      </Center>
+    </KeyboardAwareScrollView>
   )
 }
